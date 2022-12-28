20a6f78926f06b0dd98c7f54ab3b54c351c710c8
gilar setiawan <gilarsr08@gmail.com>
2022-12-29 02:33:46 +0700

    minpro selesai

--- portfilo/src/page/about/About.jsx
+++ portfilo/src/page/about/About.jsx
@@ -8,15 +8,17 @@ const About = ()=>{
             <header>
             <div className="myself">
                 <NavbarList/>
-                <dl className="me">
-                    <dt className="my">MY SELF</dt>
-                    <dd>I am a front end web developer located in Semarang, Central Java, Indonesia. 
-                        I really like design activities such as logos and images.
-                        I am a person who is disciplined and responsible for tasks, 
-                        can work in teams, and is thorough in looking at problems from various angles, 
-                        and I am interested in the entire frontend spectrum and work on ambitious projects with positive people.
-                    </dd>
-                </dl>
+                <div className="me">
+                    <dl>
+                        <dt className="my">MY SELF</dt>
+                        <dd style={{color: "azure"}}>I am a front end web developer located in Semarang, Central Java, Indonesia. 
+                            I really like design activities such as logos and images.
+                            I am a person who is disciplined and responsible for tasks, 
+                            can work in teams, and is thorough in looking at problems from various angles, 
+                            and I am interested in the entire frontend spectrum and work on ambitious projects with positive people.
+                        </dd>
+                    </dl>
+                </div>
             </div>
             </header>
         </>
--- portfilo/src/page/minpro/Minpro.js
+++ portfilo/src/page/minpro/Minpro.js
@@ -1,10 +1,12 @@
+import "./minpro.css";
 import NavbarList from "../../componen/navbar/Navbar";
 import Card from "react-bootstrap/Card";
-import foto from "../../componen/foto/the_movie.jpeg";
-// import foto from "../../componen/foto/nakama.jpeg";
+import MoveDB from "../../componen/foto/the_movie.jpeg";
+import Nakama from "../../componen/foto/nakama.jpeg";
 import { Col, Row } from "react-bootstrap";
 
 const MinPro = () => {
+
 
   return (
     <>
@@ -12,22 +14,24 @@ const MinPro = () => {
         <div>
           <NavbarList />
           <Row>
-            <Col sm="6">
+            <Col sm="6" style={{width: "auto", paddingTop: "10px"}}>
               <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src={foto} />
+                <Card.Img variant="top" src={MoveDB} />
                 <Card.Body>
                   <Card.Title>the movie DB</Card.Title>
+                  <p>this is a project i created using api from the movei db</p>
                   <a href="https://github.com/Gilarsetiawanramadhan/mini_projek_2_vilm">
                     open link
                   </a>
                 </Card.Body>
               </Card>
             </Col>
-            <Col sm="6">
+            <Col sm="6" style={{width: "auto", paddingTop: "10px"}}>
               <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src={`${foto}/nakama.jpeg`} />
+                <Card.Img variant="top" src={Nakama} />
                 <Card.Body>
                   <Card.Title>the movie DB</Card.Title>
+                  <p>this is a project i created using api from the movei db</p>
                   <a href="https://github.com/Gilarsetiawanramadhan/mini_projek_2_vilm">
                     open link
                   </a>
